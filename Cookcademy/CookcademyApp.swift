d18799b67922e60e05303ae82be858f3a40eae99
Kseniya <lev.ksusha98@gmail.com>
2024-07-31 14:34:29 -0400

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,18 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Ксения Левицкая on 6/20/24.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+  var body: some Scene {
+    WindowGroup {
+      MainTabView()
+    }
+  }
+}
+
